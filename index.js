df16b51f0f791bb4a1a6630a158a2c2f9d5f0ad1
Thomas Maxey <tmaxey201@gmail.com>
2021-06-08 11:18:14 -0400

    added prompts for eng/mng/intern imputs

--- index.js
+++ index.js
@@ -2,3 +2,38 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+
+
+const addManager = () => {
+    return new Promise((res) => {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Enter manager's name:",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "Enter manager's ID:",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "Enter manager's email:",
+                    name: "email",
+                   
+                  
+                },
+                {
+                    type: "input",
+                    message: "Enter manager's office number:",
+                    name: "officeNumber",
+                },
+        //   adding manager info into team array
+            ]).then(answers => {
+                const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+                team.push(manager);
+                res();
+            });
+    });
+}
